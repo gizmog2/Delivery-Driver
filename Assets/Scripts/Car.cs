58046d824d3a8183c900751fd553a5ec718e5acb
Сергей Буркун <GizmoG2@live.ru>
2022-01-31 18:45:17 +0200

    Тест 1.

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    [SerializeField] float rotationSpeed = 0.2f;
+    [SerializeField] float driveSpeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationAmount = Input.GetAxis("Horizontal") * rotationSpeed;
+        float speedAmount = Input.GetAxis("Vertical") * driveSpeed;
+        transform.Rotate(0, 0, -rotationAmount);
+        transform.Translate(0, speedAmount, 0);
+    }
+}
