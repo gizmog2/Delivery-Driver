ca39cd8224f9934056be511f74776f5b5dfeda64
Сергей Буркун <GizmoG2@live.ru>
2022-02-06 20:44:00 +0200

    Добавлена возможность ускорения и замедления.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -7,6 +7,9 @@ public class Car : MonoBehaviour
 
     [SerializeField] float rotationSpeed = 0.2f;
     [SerializeField] float driveSpeed = 0.01f;
+    [SerializeField] float slowSpeed = 0.1f;
+    [SerializeField] float boostSpeed = 0.4f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +22,21 @@ public class Car : MonoBehaviour
         float rotationAmount = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
         float speedAmount = Input.GetAxis("Vertical") * driveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -rotationAmount);
-        transform.Translate(0, speedAmount, 0);
-
-        
+        transform.Translate(0, speedAmount, 0);        
     }
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        driveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Boost")
+        {
+            driveSpeed = boostSpeed;
+            Destroy(collision.gameObject, 0.2f);
+        }        
+       
+    }
+
 }
